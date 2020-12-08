d50f7caf03cdc1ac2ee4750b4086ecbd236bcb7d
bradybess <bess.brady9@gmail.com>
2020-12-08 15:01:18 -0700

    Initial Push

--- TSPSolver.py
+++ TSPSolver.py
@@ -17,6 +17,7 @@ from TSPClasses import *
 import heapq
 import itertools
 from random import random as rand
+import copy
 
 
 class TSPSolver:
@@ -113,35 +114,128 @@ class TSPSolver:
         results = {}
         cities = self._scenario.getCities()
         ncities = len(cities)
-
-        def randCost(dE, T): #FIXME: Tune function
-            c_corrected = np.exp(dE/T)
-            return c_corrected > rand(0,1) #FIXME: Figure out random range
-
+        
+        #DEBUG VALUES#
+        randCostTrue = 0
+        randCostTrueAfterHalfTempDecrease = 0
+        randTrueAfterQuarterLeft = 0
+        totalIter = 0
+        ###
+        
         start_time = time.time()
         C = self.getInitialSol() #FIXME: Get initial solution
-        T, Tmin, dec_T = getT() #FIXME: Get T and T decrement function
+        T, Tmin, dec_T = self.getT() #FIXME: Get T and T decrement function
+        init_T = T
         while T > Tmin:
-            C_n = self.getNeighbor(C) #FIXME: Implement
+            C_n, iterations = self.getNeighbor(C) #FIXME: Implement
+            print("TIMES IT TOOK TO FIND NEIGHBOR: " + str(iterations))
+            totalIter += iterations
             if C_n.cost != np.inf:
                 delta_cost = C_n.cost - C.cost
                 if delta_cost < 0:
                     C = C_n
-                elif randCost(delta_cost,T): 
+                elif self.randCost(delta_cost,T): 
+                    randCostTrue += 1
+                    
+                    if (T < init_T/2): #start debug value counting for updates past halfway done
+                        randCostTrueAfterHalfTempDecrease += 1
+                        
+                    if (T < init_T/4):
+                        randTrueAfterQuarterLeft += 1
+                    
                     C = C_n
-            T = dec_T(T)
+            T -= dec_T
+            
         end_time = time.time()
+        
+        print("AVERAGE ITER TO FIND NEIGHBOR = " + str(totalIter/(T/dec_T)))
+        print("TIMES PROBABILITY WON: " + str(randCostTrue))
+        print("TIMES PROBABILITY WON (MORE THAN HALFWAY THRU): " + str(randCostTrueAfterHalfTempDecrease))
+        print("TIMES PROBABILITY WON (Last QUARTER): " + str(randTrueAfterQuarterLeft))
+
+    
 
         pass
         
-    def getNeighbor(self,C):
-        pass
+    def getNeighbor(self, C):
+        for i in range(len(C.route)):
+            print(C.route[i]._index, end="")
+        print("\n")
+        
+        iterations = 0
+            
+        foundNeighbor = False
+        while not foundNeighbor:
+            iterations += 1
+            randPlaceToBeginSwap = random.randint(0, len(C.route) - 2)
+            randPlaceToEndSwap = random.randint(randPlaceToBeginSwap + 1, len(C.route) - 1)
+            
+            pathToAlter = copy.copy(C.route)
+            
+            slicePath = pathToAlter[randPlaceToBeginSwap:randPlaceToEndSwap + 1]
+            for i in slicePath:
+                print(i._index, end="")
+            print("\n")
+            
+            pathToAlter[randPlaceToBeginSwap:randPlaceToEndSwap + 1] = reversed(pathToAlter[randPlaceToBeginSwap:randPlaceToEndSwap + 1])
+            
+            possibleNeighbor = TSPSolution(pathToAlter)
+            
+            for i in range(len(possibleNeighbor.route)):
+                print(possibleNeighbor.route[i]._index, end="")
+            print("\n")
+            
+            if (possibleNeighbor.cost != np.inf):
+                foundNeighbor = True
+                
+        return possibleNeighbor, iterations
+    
+    #8 cities, 5000/5 Ts, 2.774 average iterations to find neighbor
+    #10 cities, 5000/50 Ts, 3.8 average iter to find neighbor
+    #10 cities, 10000/100 Ts, 2.94 average iter
+    #15 cities, 5000/50 Ts, 5.26 average iter to find neighbor
+    #15 cities, 10000/100 Ts, 3.87 average iter
+    #15 cities, 50000/500 Ts, 4.17 average iter
 
     def getT(self):
-        pass
+        return 10000, 1, 100
+    
+    
+    #FINDINGS/NOTES:
+    #The smaller the initial T, the less ability it will have to "jump" localities (smaller denominators)
+    #(Obvious) the larger diff between T and Tmin the more iterations
+    #The larger the dec_T the more we can reduce iterations and maintain high Ts still
+    
+    def randCost(self, dE, T): #FIXME: Tune function
+        c_corrected = np.exp(-dE/T)
+        randNum = random.random()
+        print("C_CORRECTED = " + str(c_corrected))
+        return c_corrected > randNum #FIXME: Figure out random range
+    
+    #15 cities, 5000/50 Ts, 36 times randCost eval to True
+    #15 cities, 1000/10 Ts, 19 times randCost eval to True
+    #10 cities, 10000/100 Ts, 41 times eval to True
+    #15 cities, 10000/100 Ts, 37 times randCost eval to True
+    #15 cities, 10000/100 Ts, 43 times eval to True
+    
+    #15 cities, 10000/100 Ts, 41 times eval to True, 18 after 50% completion
+    #15 cities, 50000/500 Ts, 48 times eval to True, 21 after 50% completion
+    
+    #15 cities, 1000/10 Ts, (15,23) times eval to True, (1, 10) after half, (0, 4) with 1/4 left
+    #15 cities, 5000/50 Ts, (35, 28) times eval to True, (14, 11) after half, (5, 4) with 1/4 left
+    #15 cities, 10000/100 Ts, (38 40) times eval to True, (16, 21) after half, (7,10) with 1/4 left  ###TOO HIGH
+    #15 cities, 50000/500 Ts, 51 times eval to True, 26 after half, 12 with 1/4 left ###TOO HIGH
+    
+    
+    ###TAKEAWAYS### With  higher T and dec_T values the randCost function won't actually restrict values
+    #as the function gets closer to finishing
+    
+    
+
+
 
     def getInitialSol(self):
         #returns TSPSolution
-        pass
+        return self.defaultRandomTour()["soln"]
 
 
